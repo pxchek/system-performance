fb5ceb12f5f1581b2418313b5201fe80731466e9
Panindra Chekoori <pxchek.developer@gmail.com>
2022-11-13 22:25:53 -0600

    Polishing

--- src/main/java/collections/SortBenchmark.java
+++ src/main/java/collections/SortBenchmark.java
@@ -17,52 +17,16 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.Throughput)
-@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 1, time = 1)
+@Measurement(iterations = 1, time = 1)
 @OutputTimeUnit(TimeUnit.SECONDS)
 @Fork(1)
 public class SortBenchmark {
-//    private static final int N = 1_000;
-//    private static final List<Integer> testData = new ArrayList<>();
-//
-//    public static final void setup() {
-//        Random randomGenerator = new Random();
-//        for(int i = 1; i <N;  i++) {
-//            testData.add(randomGenerator.nextInt(Integer.MAX_VALUE));
-//            System.out.println("Setup Complete.");
-//        }
-//    }
-//
-//    @Benchmark
-//    public List<Integer> classicSort() {
-//        List<Integer> copy = new ArrayList<>(testData);
-//        Collections.sort(copy);
-//        return copy;
-//    }
-//
-//    @Benchmark
-//    public List<Integer> standardSort() {
-//        return testData.stream()
-//                .sorted()
-//                .collect(Collectors.toList());
-//    }
-//
-//    @Benchmark
-//    public List<Integer> parallelSort() {
-//        return testData
-//                .parallelStream()
-//                .sorted()
-//                .collect(Collectors.toList());
-//    }
-
     @Benchmark
     public List<String> properlySizedArrayList() {
         List<String> list = new ArrayList<>(1_000_000);
@@ -85,7 +49,7 @@ public class SortBenchmark {
         Options opt = new OptionsBuilder()
                 .include(SortBenchmark.class.getSimpleName())
                 .warmupIterations(5)
-                .measurementIterations(5)
+                .measurementIterations(1)
                 .forks(1)
                 .jvmArgs("-server", "-Xms2048m", "-Xmx2048m")
                 .addProfiler(GCProfiler.class)
